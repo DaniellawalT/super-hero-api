d2b08e203d9c52414fe509f71732228a7036c233
Daniel lawal <Daniellawal22@yahoo.com>
2024-07-25 11:37:53 +0100

    Initial implementation of Super Hero API with CRUD operations

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNET8.Entities;
+
+namespace SuperHeroAPI_DotNET8.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
